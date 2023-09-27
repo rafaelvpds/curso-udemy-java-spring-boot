68da96d2425b87d3883c65117e6151b4f3c3ced9
Rafael <rafaelvpds.dev09@gmail.com>
2023-09-27 11:46:16 -0300

    Json Serialization

--- src/main/java/com/cursoudemy/persons/models/dto/dtoV01/PersonDto.java
+++ src/main/java/com/cursoudemy/persons/models/dto/dtoV01/PersonDto.java
@@ -1,5 +1,10 @@
 package com.cursoudemy.persons.models.dto.dtoV01;
 
-public record PersonDto(Long id, String firstName, String lastName, String address, Character gender) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({ "id", "firstName", "lastName", "gender", "address" })
+public record PersonDto(Long id, @JsonProperty("first_name") String firstName,
+        @JsonProperty("last_name") String lastName, String address, Character gender) {
 
 }
